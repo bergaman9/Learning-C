4f008431e763559136d8a2e01448a6527d0f6e24
bergaman9 <omerguler53@gmail.com>
2022-10-30 19:17:25 +0300

    some changes

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main () {
     int a = 5, b = 3;
-    printf("a + b = %d", a+b); 
-    printf("b - a = %d", b-a);
+    printf("a + b = %d\n", a+b); 
+    printf("b - a = %d\n", b-a);
+    return 0;
 }
---
+++ square-root.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number to find square root of it: ");
+    scanf("%d", &num);
+
+    printf("The root of %d is %f", num, (double)sqrt(num));
+
+
+    return 0;
+}
